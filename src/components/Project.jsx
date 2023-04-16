56a5ca6d8e674ef7379e3ead119ba0d229b6247a
SauravKumar <sk729584@gmail.com>
2023-04-16 13:14:19 +0530

    Change link of some project and add a project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,5 @@
 import myglamm from "../assets/portfolio/navbar.png";
-import bigbasket from "../assets/portfolio/reactWeather.jpg";
 import myntra from "../assets/portfolio/myntra.jpg";
-import nykaa from "../assets/portfolio/nykaa.png";
 
 import { FaGithub } from "react-icons/fa";
 import { SiNetlify } from "react-icons/si";
@@ -21,42 +19,52 @@ const Project = () => {
   const portfolios = [
     {
       id: 1,
-      src: bigbasket,
+      src: "https://user-images.githubusercontent.com/104342116/224603504-899f47b6-9a83-465b-ae52-54c4529a42c9.png",
       github: "https://github.com/Saurav02022/BigBasket-Clone",
-      netlify: "https://bigbasket-apnidukan.netlify.app",
-      websiteName: "Bigbasket Clone",
-      des: "It is an E-commerce website for online supermarket of all your daily needs.",
+      netlify: "https://bigbasketclone.saurav02022.vercel.app/",
+      websiteName: "BigBasket Clone",
+      des: "The goal of this project was to recreate the essential features of BigBasket.com, such as product listings, a shopping cart, checkout, and payment. The project was developed as an individual project and took 5 days to complete. The project features responsive design for a seamless shopping experience on all devices.",
       TechStack: [reactImage, Redux, Chakra],
       Area: "Area of responsibility :- An individual project",
     },
     {
       id: 2,
-      src: nykaa,
+      src: "https://user-images.githubusercontent.com/104342116/227706094-dde6962c-89ec-4209-aae0-9ff7f5b96f2b.png",
+      github: "https://github.com/Saurav02022/Task-Planner",
+      netlify: "https://taskplanner.saurav02022.vercel.app/login",
+      websiteName: "Task-Planner",
+      des: "It is a Task-Planner,it allows users to Create tasks, change their status, and view progress to Improve team productivity with this easy-to-use tool.also user can create sprint ,delete sprint,update sprint name.",
+      TechStack: [reactImage, Redux, Chakra, node, express, mongo],
+      Area: "Area of responsibility :- An individual project",
+    },
+    {
+      id: 3,
+      src: "https://user-images.githubusercontent.com/104342116/213980455-c8c6e7a5-3e17-4e40-bee0-0e211a835752.png",
       github: "https://github.com/Saurav02022/Nykaa-Clone",
-      netlify: "https://frontend-three-coral.vercel.app/",
+      netlify: "https://nykaaclone.saurav02022.vercel.app/",
       websiteName: "Nykaa Clone",
-      des: "Nykaa is an e-commerce website that sells beauty, wellness, and fashion products etc.",
+      des: "Nykaa is an e-commerce website that sells beauty, wellness, and fashion products etc.This clone was created to showcase my skills in building complex e-commerce applications and to provide a seamless shopping experience for users.",
       TechStack: [reactImage, Redux, Chakra, node, express, mongo],
       Area: "Area of responsibility :- Home Page,Cart Page and Payment Page",
     },
     {
-      id: 3,
+      id: 4,
       src: myntra,
       github: "https://github.com/harshau9/eager-grain-3783",
       netlify: "https://myntra-gilt.vercel.app",
       websiteName: "Myntra Clone",
-      des: "Myntra is an e-commerce website that sells cosmetics and personal care products etc.",
+      des: "Myntra Clone by using ReactJS. It is an Indian online fashion and lifestyle e-commerce company. It sells clothing, footwear, and accessories for men, women, and children. The main goal of the project is to gain teamwork experience. The UI is designed in the way so that it resembles with the Myntra website.",
       TechStack: [reactImage, Redux, Chakra],
       Area: "Area of responsibility :- Signup Page,Login Page and Admin Page",
     },
 
     {
-      id: 4,
+      id: 5,
       src: myglamm,
       github: "https://github.com/Saurav02022/languid-smash-8138",
       netlify: "https://bright-moxie-fed954.netlify.app",
       websiteName: "myglamm Clone",
-      des: "MyGlamm is an e-commerce website that sells cosmetics and personal care products.",
+      des: "I created a clone of myglamm.com's home, signup, login and one product pages using HTML, CSS,Bootstrap and JavaScript.This was an individual project that I completed and pushed onto GitHub for others to view and evaluate. My clone project is intended to showcase my skills in development and ability to replicate a real-world website.",
       TechStack: [html, css, javascript, Bootsrap],
       Area: "Area of responsibility :- An individual project",
     },
@@ -98,7 +106,9 @@ const Project = () => {
                   className="rounded-md duration-200 hover:scale-105"
                 />
                 <h2 className="text-3xl mt-3 text-center">{websiteName}</h2>
-                <h2 className="text-xl mt-4 text-center">{des}</h2>
+                <h2 className="text-xl mt-4 text-center leading-tight">
+                  {des}
+                </h2>
                 <div className="flex gap-4 items-center justify-center mt-4 ">
                   {TechStack.map((data, i) => (
                     <img
