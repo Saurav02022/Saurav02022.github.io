d75c29beb8c50b092369a86957c4901f958f6bdd
SauravKumar <sk729584@gmail.com>
2024-09-24 20:31:31 +0530

    Added Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import About from "./components/About";
-import Skills from "./components/Skills";
+import Info from "./components/Contact";
+import GitHubCalendarComponent from "./components/GithubCalender";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Project from "./components/Project";
-import GitHubCalendarComponent from "./components/GithubCalender";
-import Info from "./components/Contact";
+import Skills from "./components/Skills";
+
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-2BR0PH5SXP");
 
 function App() {
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "Portfolio visit",
+      page: "/",
+    });
+  }, []);
   return (
     <>
       <NavBar />
